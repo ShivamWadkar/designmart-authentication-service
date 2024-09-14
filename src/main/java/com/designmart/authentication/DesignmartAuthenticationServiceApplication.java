aa82a757fe0168f86f1fd196678137fbf0b6f583
ShivamWadkar <shivamvwadkar@gmail.com>
2024-09-14 23:28:30 +0530

    Added initial template

---
+++ src/main/java/com/designmart/authentication/DesignmartAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.designmart.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignmartAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignmartAuthenticationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/designmart/authentication/DesignmartAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designmart.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignmartAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
